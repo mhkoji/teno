0be69c53c144202b16c219c6c7327d97cac25e29
mhkoji <mhkoji2+git@gmail.com>
2022-07-18 12:11:06 +0900

    Delete everything

--- README.md
+++ README.md
@@ -1,7 +1,1 @@
 # teno
-
-## Init
-
-```
-docker run --network=teno_default --entrypoint "" -it teno_webapp /app/webapp init
-```
--- webapp/server/src/markdown.lisp
+++
@@ -1,8 +0,0 @@
-(defpackage teno.markdown
-  (:use :cl)
-  (:export :to-html))
-(in-package :teno.markdown)
-
-(defun to-html (string)
-  (with-output-to-string (s)
-    (cl-markdown:markdown string :stream s)))
